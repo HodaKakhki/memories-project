8231b7ad33ed928455a22d5f189d5859e3bbbebd
hodhodi <amis883@yahoo.com>
2024-03-11 14:29:05 +0330

    set up redux

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import { createStore, applymiddleware, compose } from ' redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './App';
-
+const store =createStore(reducers,compose(applymiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />          
+    <Provider store={store}>
+          <App />
+  </Provider>          
 );
 
