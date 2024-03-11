803b8060f232ce051b5c0e5e2e04f9246a5e782f
hodhodi <amis883@yahoo.com>
2024-03-11 09:46:26 +0330

    adding styles

--- front/src/components/Posts/Posts.js
+++ front/src/components/Posts/Posts.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Post from "./Post/Post.js";
+import useStyles from "./styles"
+
+
 function Posts() {
+  const classes = useStyles();
   return (
     <>
       <h1>POSTS</h1>
